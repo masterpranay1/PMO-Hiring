8228e2b394073b5445836fb8e301c31504a9bdf1
masterpranay1 <rajpranaychottu@gmail.com>
2023-09-13 12:52:52 +0530

    Update : Navbar ( Links )

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,8 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <nav className="flex flex-row items-center bg-white px-8 py-4 border-b-2">
-      <h2 className="text-xl">PMO Project</h2>
-      <button className="bg-red-700 text-slate-100 p-2 rounded-md ml-auto hover:bg-red-400">
+      <h2
+        className="text-xl cursor-pointer"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        PMO Project
+      </h2>
+      <button className="bg-red-700 text-slate-100 p-2 rounded-md ml-auto hover:bg-red-400"
+      onClick={() => {
+        navigate("/register");
+      }}>
         Join us
       </button>
     </nav>
