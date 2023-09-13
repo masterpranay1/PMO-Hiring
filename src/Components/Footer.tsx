4844544015155f1f577bb3a0973d5d6e4b67684c
masterpranay1 <rajpranaychottu@gmail.com>
2023-09-13 12:01:32 +0530

    Feat : Footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+      <footer>
+          {/* Generate the copyright icon */}
+          <div className="text-center p-8 bg-red-50 border-t-2">
+              <p className="text-red-700">&copy;2021 PMO Project</p>
+          </div>
+      </footer>
+  )
+}
+
+export default Footer;
