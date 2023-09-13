b437c733d54c8cd963a85d1b8787e9d96cf4726a
masterpranay1 <rajpranaychottu@gmail.com>
2023-09-13 12:53:09 +0530

    Update : App ( Register, Login Route )

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </>
   )
