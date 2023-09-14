326b6a59b7219a608f6688c507d11b0651d24bc6
masterpranay1 <rajpranaychottu@gmail.com>
2023-09-14 08:40:24 +0530

    Fix : JobCard

--- src/Components/JobCard.tsx
+++ src/Components/JobCard.tsx
@@ -24,7 +24,8 @@ const JobCard = ({
       <a
         href="#"
         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 mt-auto w-fit"
-        onClick={() => {
+        onClick={(e) => {
+          e.preventDefault();
           navigate("/hiring-form?type=" + title);
         }}
       >
