6ff81fb279ad77c0a8f5566d4c10d2091e23bf34
Pranay Raj <58875281+masterpranay1@users.noreply.github.com>
2023-09-18 04:36:38 +0000

    Fix : Login ( color typo )

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -51,7 +51,7 @@ const LoginForm = ({
   }
 
   return (
-    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-red-600 dark:border-red-700">
+    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-600 dark:border-gray-700">
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
         <h1 className="text-xl font-bold leading-tight tracking-tight text-red-600 md:text-2xl dark:text-white">
           Sign in to your account
